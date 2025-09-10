9441250725088c61388494a41599f7779da2c565
LianXia233 <49828512+LianXia233@users.noreply.github.com>
2025-09-10 09:24:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,22 +9,22 @@ echo "=== Start DIY Script ==="
 
 # 函数：更新或安装指定插件
 UPDATE_PACKAGE() {
-    local PKG_NAME=$1
-    local PKG_REPO=$2
-    local PKG_BRANCH=$3
-    local PKG_SPECIAL=$4
-    local REPO_NAME=${PKG_REPO#*/}
+    local PKG_NAME="$1"
+    local PKG_REPO="$2"
+    local PKG_BRANCH="$3"
+    local PKG_SPECIAL="$4"
+    local REPO_NAME="${PKG_REPO#*/}"
 
     # 删除本地可能存在的同名包
-    find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -exec rm -rf {} \;
+    find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*${PKG_NAME}*" -exec rm -rf {} +
 
     # 克隆 GitHub 仓库
-    git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+    git clone --depth=1 --single-branch --branch "$PKG_BRANCH" "https://github.com/${PKG_REPO}.git"
 
     # 如果是 pkg，则从大杂烩中提取插件目录
-    if [[ $PKG_SPECIAL == "pkg" ]]; then
-        find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
-        rm -rf ./$REPO_NAME/
+    if [[ "$PKG_SPECIAL" == "pkg" ]]; then
+        find "./${REPO_NAME}/"*/ -maxdepth 3 -type d -iname "*${PKG_NAME}*" -prune -exec cp -rf {} ./ \;
+        rm -rf "./${REPO_NAME}/"
     fi
 }
 
@@ -42,8 +42,8 @@ UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "main" "pkg"
 if ! grep -q "src-git modem" feeds.conf.default; then
     echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 fi
-./scripts/feeds update modem
-./scripts/feeds install -a -p modem
-./scripts/feeds install -a -f -p modem
+./scripts/feeds update modem || true
+./scripts/feeds install -a -p modem || true
+./scripts/feeds install -a -f -p modem || true
 
 echo "=== DIY Script Finished ==="
